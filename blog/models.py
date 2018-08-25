cd1d04b2de6b7b1b98988e5f7d1afe53eb0af9a7
Rafal <rrudzin1@asu.edu>
2018-08-25 15:33:28 -0700

    added blog, made changes to header

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 
 class Blog(models.Model):
+    objects = models.Manager()
     title = models.CharField(max_length=255)
     pub_date = models.DateField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %y, %Y')
--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class Job(models.Model):
+    objects = models.Manager()
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
