2229055807ea0841dfc162d61d3503d585a30b9d
Rafal <rrudzin1@asu.edu>
2018-08-24 22:19:23 -0700

    migrated Job app'

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Br00k3Akem!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
